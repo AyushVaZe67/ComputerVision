51bc5d6f0420d80ec49cea08629118cbd434c4a5
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-26 01:03:15 +0530

    Add files via upload

---
+++ Chapter1 - Basics/Chap1-HandTrackingBasics.py
@@ -0,0 +1,15 @@
+import cv2
+import time
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+while True:
+    sucess,img = cap.read()
+    cv2.imshow('Image',img)
+    cv2.waitKey(1)
+
+print('Ayush')
