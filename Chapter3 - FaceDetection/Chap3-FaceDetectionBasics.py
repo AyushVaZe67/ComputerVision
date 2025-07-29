ba5fc0d38c7e2c0782e692c42ae7752dcbb575b4
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-29 18:31:25 +0530

    Add files via upload

--- Chapter3 - FaceDetection/Chap3-FaceDetectionBasics.py
+++ Chapter3 - FaceDetection/Chap3-FaceDetectionBasics.py
@@ -2,11 +2,14 @@ import cv2
 import time
 import mediapipe as mp
 
-cap = cv2.VideoCapture('face_detection1')
+cap = cv2.VideoCapture('face_detection1.mp4')
+pTime = 0
 
 while True:
     success, img = cap.read()
-    cv2.imshow('img',img)
     cTime = time.time()
-    fps = 1(cTime-pTime)
+    fps = 1/(cTime-pTime)
     pTime = cTime
+    cv2.putText(img,f'{int(fps)}',(20,70),cv2.FONT_HERSHEY_PLAIN,3,(255,255,0))
+    cv2.imshow('img', img)
+    cv2.waitKey(10)
