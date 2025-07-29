cd72bd4325a2efb1eefeed2a8706bcbd9084b360
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-29 18:36:39 +0530

    Add files via upload

--- Chapter3 - FaceDetection/Chap3-FaceDetectionBasics.py
+++ Chapter3 - FaceDetection/Chap3-FaceDetectionBasics.py
@@ -5,11 +5,20 @@ import mediapipe as mp
 cap = cv2.VideoCapture('face_detection1.mp4')
 pTime = 0
 
+mpFaceDetection = mp.solutions.face_detection
+mpDraw = mp.solutions.drawing_utils
+faceDetection = mpFaceDetection.FaceDetection()
+
 while True:
     success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceDetection.process(imgRGB)
+    print(results)
+
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
-    cv2.putText(img,f'{int(fps)}',(20,70),cv2.FONT_HERSHEY_PLAIN,6,(0,0,0))
+    cv2.putText(img,f'FPS:{int(fps)}',(20,70),cv2.FONT_HERSHEY_PLAIN,6,(0,0,0),5)
     cv2.imshow('img', img)
-    cv2.waitKey(10)
+    cv2.waitKey(1)
