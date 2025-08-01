ad035579ba7d634b48a0fbf48c88186a0255ace1
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-08-01 10:19:42 +0530

    Add files via upload

--- Chapter4 - FaceMesh/Chap4-FaceMeshModule.py
+++ Chapter4 - FaceMesh/Chap4-FaceMeshModule.py
@@ -4,3 +4,48 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import time
 import mediapipe as mp
 import cv2
+
+
+class FaceMeshDetector:
+    def __init__(self,static_mode=False,maxFaces=2,minDetectionCon=0.5,minTrackCon=0.5):
+        self.static_mode = static_mode
+        self.maxFaces = maxFaces
+        self.minDetectionCon = minDetectionCon
+        self.minTrackCon = minTrackCon
+
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpFaceMesh = mp.solutions.face_mesh
+        self.faceMesh = self.mpFaceMesh.FaceMesh(self.static_mode,self.maxFaces,self.minDetectionCon,self.minTrackCon)
+        self.drawSpec = self.mpDraw.DrawingSpec(thickness=1,circle_radius=1)
+
+
+    # imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    # results = faceMesh.process(imgRGB)
+    # if results.multi_face_landmarks:
+    #     for faceLms in results.multi_face_landmarks:
+    #         mpDraw.draw_landmarks(img,faceLms,mpFaceMesh.FACEMESH_CONTOURS,drawSpec,drawSpec)
+    #         for id,lm in enumerate(faceLms.landmark):
+    #             # print(lm)
+    #             ih,iw,ic = img.shape
+    #             x,y = int(lm.x*iw),int(lm.y*ih)
+    #             print(id,x,y)
+    #
+
+
+def main():
+    cap = cv2.VideoCapture('face_mesh1.mp4')
+    pTime = 0
+    while True:
+        success, img = cap.read()
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), 2)
+        cv2.imshow('Image', img)
+        cv2.waitKey(1)
+
+if __name__ == '__main__':
+    main()
