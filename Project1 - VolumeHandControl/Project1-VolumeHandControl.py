d546f6d7efbc53c9858daf6aacc060492ba9fa02
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-08-01 10:55:29 +0530

    Add files via upload

--- Project1 - VolumeHandControl/Project1-VolumeHandControl.py
+++ Project1 - VolumeHandControl/Project1-VolumeHandControl.py
@@ -9,17 +9,22 @@ import HandTrackingModule as htm
 cap = cv2.VideoCapture(0)
 pTime = 0
 
-detector = htm.HandDetector
+detector = htm.HandDetector()
 
 while True:
-    success,img = cap.read()
-
+    success, img = cap.read()
     detector.findHands(img)
+    if not success or img is None:
+        print("Failed to capture frame.")
+        continue
 
+    # FPS calculation
     cTime = time.time()
-    fps = 1/(cTime-pTime)
+    fps = 1 / (cTime - pTime)
     pTime = cTime
 
     cv2.putText(img, f'FPS: {fps:.2f}', (40, 50), cv2.FONT_ITALIC, 1, (0, 0, 0), 3)
-    cv2.imshow('Image',img)
-    cv2.waitKey(1)
+    cv2.imshow('Image', img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
