37efaf351648b935bd0b1df40dfeb831ad2418ed
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-08-03 13:20:10 +0530

    Add files via upload

--- Project1 - VolumeHandControl/Project1-VolumeHandControl.py
+++ Project1 - VolumeHandControl/Project1-VolumeHandControl.py
@@ -32,8 +32,11 @@ while True:
         x1,y1 = lmList[4][1],lmList[4][2]
         x2,y2 = lmList[8][1],lmList[8][2]
 
-        cv2.circle(img,(x1,y1),15,(255,0,255),cv2.FONT_HERSHEY_PLAIN,2)
-        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FONT_HERSHEY_PLAIN, 2)
+        cx,cy = (x1+x2)//2,(x2+y2)//2
+        cv2.circle(img,(x1,y1),15,(0,255,0),cv2.FONT_HERSHEY_PLAIN)
+        cv2.circle(img, (x2, y2), 15, (0,255,0), cv2.FONT_HERSHEY_PLAIN)
+        cv2.line(img,(x1,y1),(x2,y2),(255,0,0),3)
+        cv2.circle(img,(cx,cy),15,(255,0,55),cv2.FILLED)
 
     # FPS calculation
     cTime = time.time()
